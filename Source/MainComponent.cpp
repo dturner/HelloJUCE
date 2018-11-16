ef11cf5b911874ca77aaf6777e45558f3ed4f7af
Don Turner <donturner@google.com>
2018-11-16 10:44:40 +0000

    Initial commit

---
+++ Source/MainComponent.cpp
@@ -0,0 +1,74 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+  ==============================================================================
+*/
+
+#include "MainComponent.h"
+
+//==============================================================================
+MainComponent::MainComponent()
+{
+    // Make sure you set the size of the component after
+    // you add any child components.
+    setSize (800, 600);
+
+    // specify the number of input and output channels that we want to open
+    setAudioChannels (2, 1);
+    
+    osc.setWaveOn(true);
+    osc.setAmplitude(0.3);
+    osc.setFrequency(140.0);
+}
+
+MainComponent::~MainComponent()
+{
+    // This shuts down the audio device and clears the audio source.
+    shutdownAudio();
+}
+
+//==============================================================================
+void MainComponent::prepareToPlay (int samplesPerBlockExpected, double sampleRate)
+{
+    // This function will be called when the audio device is started, or when
+    // its settings (i.e. sample rate, block size, etc) are changed.
+
+    // You can use this function to initialise any resources you might need,
+    // but be careful - it will be called on the audio thread, not the GUI thread.
+
+    // For more details, see the help for AudioProcessor::prepareToPlay()
+}
+
+void MainComponent::getNextAudioBlock (const AudioSourceChannelInfo& bufferToFill)
+{
+    for (int channel = 0; channel < bufferToFill.buffer->getNumChannels(); ++channel){
+        float * const buffer = bufferToFill.buffer->getWritePointer(channel, bufferToFill.startSample);
+        osc.renderAudio(buffer, bufferToFill.numSamples);
+    }
+}
+
+void MainComponent::releaseResources()
+{
+    // This will be called when the audio device stops, or when it is being
+    // restarted due to a setting change.
+
+    // For more details, see the help for AudioProcessor::releaseResources()
+}
+
+//==============================================================================
+void MainComponent::paint (Graphics& g)
+{
+    // (Our component is opaque, so we must completely fill the background with a solid colour)
+    g.fillAll (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));
+
+    // You can add your drawing code here!
+}
+
+void MainComponent::resized()
+{
+    // This is called when the MainContentComponent is resized.
+    // If you add any child components, this is where you should
+    // update their positions.
+}
